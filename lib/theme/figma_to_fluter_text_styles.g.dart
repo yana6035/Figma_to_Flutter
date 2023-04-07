d38ab32dde43cc2b2a1d15c282bc9a2aeb27342a
Parabeac-Cloud <admin@parabeac.com>
2023-04-07 14:33:33 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/figma_to_fluter_text_styles.g.dart
@@ -0,0 +1,40 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class FigmaToFluterTextStyles {
+
+static const TextStyle 38Bold = TextStyle(
+  fontSize: 38,
+  fontWeight: FontWeight.w700,
+  letterSpacing: -0.76000009059906,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle 16Semibold = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  letterSpacing: -0.32000003814697264,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle 18Regular = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+  letterSpacing: -0.3600000429153442,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
